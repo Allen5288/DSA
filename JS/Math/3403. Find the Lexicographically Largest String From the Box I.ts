135200e4e075728b17bb97104582c42a7e4ef333
Allen5288 <yuanxin_xie_ee@163.com>
2025-06-04 20:18:43 +1000

    20250406-3403

---
+++ JS/Math/3403. Find the Lexicographically Largest String From the Box I.ts
@@ -0,0 +1,20 @@
+/**
+ * @param {string} word
+ * @param {number} numFriends
+ * @return {string}
+ */
+function answerString(word: string, numFriends: number): string {
+    if (numFriends === 1) {
+        return word;
+    }
+    const n = word.length;
+    let res = "";
+    for (let i = 0; i < n; i++) {
+        const s = word.substring(i, Math.min(i + n - numFriends + 1, n));
+        if (s > res) {
+            res = s;
+        }
+    }
+
+    return res;
+}
